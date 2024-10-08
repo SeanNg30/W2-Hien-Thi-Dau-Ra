34db1f86c7f056eff92bf74e79cc6bdf929bd20d
Sean <ng.hoangson93@gmail.com>
2024-10-08 14:34:17 +0700

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+function Bai1(){
+    alert('Xin chào!');}
+
+function Bai2(){
+    document.getElementById('demo1').innerHTML = 'Xin chào!';}
+
+function Bai3(){
+    document.write('Xin chào!');}
+
+function Bai4(){
+    console.log('Xin chào!');}
+
+
